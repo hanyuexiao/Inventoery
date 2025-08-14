99e36938a9683c55d252b73f4e059d4d3d28229d
hanyuexiao <91308791+hanyuexiao@users.noreply.github.com>
2025-08-14 16:14:44 +0800

    Add files via upload

---
+++ Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Inventory/Spatial/Inv_SpatialInventory.h"
+#include "Widgets/Inventory/Spatial/Inv_InventoryGrid.h"
+#include "Components/Button.h"
+
+void UInv_SpatialInventory::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	Button_Equippables->OnClicked.AddDynamic(this,&ThisClass::ShowEquippables);
+	Button_Comsumables->OnClicked.AddDynamic(this,&ThisClass::ShowComsumables);
+	Button_Craftables->OnClicked.AddDynamic(this,&ThisClass::ShowCraftables);
+}
+
+void UInv_SpatialInventory::ShowEquippables()
+{
+	SetActiveGrid(Grid_Equippables,Button_Equippables);
+}
+
+void UInv_SpatialInventory::ShowComsumables()
+{
+	SetActiveGrid(Grid_Consumables,Button_Comsumables);
+}
+
+void UInv_SpatialInventory::ShowCraftables()
+{
+	SetActiveGrid(Grid_Craftables,Button_Craftables);
+}
+
+void UInv_SpatialInventory::DisableButton(UButton* Button)
+{
+	Button_Equippables->SetIsEnabled(true);
+	Button_Comsumables->SetIsEnabled(true);
+	Button_Craftables->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
+}
+
+void UInv_SpatialInventory::SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button)
+{
+	DisableButton(Button);
+
+	Switcher->SetActiveWidget(Grid);
+}
---
+++ Source/Inventory/Public/Inventorymanagement/Components/Inv_InventoryComponent.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inv_InventoryComponent.generated.h"
+
+
+class UInv_InventoryBase;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent),Blueprintable)
+class INVENTORY_API UInv_InventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UInv_InventoryComponent();
+	
+	void ToggleInventoryMenu();
+protected:
+	virtual void BeginPlay() override;
+
+
+	
+private:
+	TWeakObjectPtr<APlayerController> OwningController;
+	
+	void ConstructInventory();
+	
+	UPROPERTY()
+	TObjectPtr<UInv_InventoryBase> InventoryMenu;
+	
+	UPROPERTY(EditAnywhere,Category="Inventory")
+	TSubclassOf<UInv_InventoryBase> InventoryMenuClass;
+
+	//-----------------------Have Open The Inventory? ------------------------//
+	
+	bool bInventoryMenuOpen = true;
+	
+	void OpenInventoryMenu();
+
+	void CloseInventoryMenu();
+	
+	//-------------------------------------------------------------------------//
+};
